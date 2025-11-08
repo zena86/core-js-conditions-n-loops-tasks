5969ccaa182a0c60a5e62e87f68a402282d39a00
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-08 22:31:09 +0400

    feat: solve convertNumberToString

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -164,8 +164,61 @@ function convertToRomanNumerals(num) {
  *  '10,5'    => 'one zero point five'
  *  '1950.2'  => 'one nine five zero point two'
  */
-function convertNumberToString(/* numberStr */) {
-  throw new Error('Not implemented');
+function convertNumberToString(numberStr) {
+  let result = '';
+
+  for (let i = 0; i < numberStr.length; i += 1) {
+    const symbol = numberStr[i];
+    let word;
+
+    switch (symbol) {
+      case '0':
+        word = 'zero';
+        break;
+      case '1':
+        word = 'one';
+        break;
+      case '2':
+        word = 'two';
+        break;
+      case '3':
+        word = 'three';
+        break;
+      case '4':
+        word = 'four';
+        break;
+      case '5':
+        word = 'five';
+        break;
+      case '6':
+        word = 'six';
+        break;
+      case '7':
+        word = 'seven';
+        break;
+      case '8':
+        word = 'eight';
+        break;
+      case '9':
+        word = 'nine';
+        break;
+      case '-':
+        word = 'minus';
+        break;
+      case '.':
+      case ',':
+        word = 'point';
+        break;
+      default:
+        word = '';
+    }
+
+    if (word) {
+      result += (result ? ' ' : '') + word;
+    }
+  }
+
+  return result;
 }
 
 /**
