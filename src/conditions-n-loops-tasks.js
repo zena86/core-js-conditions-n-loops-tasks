eb3410c186da027c2d21a8c710315078d632efef
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-08 21:44:31 +0400

    feat: solve isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -97,8 +97,11 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a + b <= c || a + c <= b || b + c <= a) {
+    return false;
+  }
+  return a === b || b === c || a === c;
 }
 
 /**
