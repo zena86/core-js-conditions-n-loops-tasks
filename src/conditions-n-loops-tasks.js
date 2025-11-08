751556515979757d59d3b4bdfd750c8917dd33a1
Yauheniya Mastavaya <jk.mostovaya@gmail.com>
2025-11-08 18:14:01 +0400

    feat: solve getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,20 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let max;
+
+  if (a > b) {
+    max = a;
+  } else {
+    max = b;
+  }
+
+  if (c > max) {
+    max = c;
+  }
+
+  return max;
 }
 
 /**
